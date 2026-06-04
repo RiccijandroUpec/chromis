fc71351b5d00338f9becf0e43d1db14d90790cf9
RiccijandroUpec <richard.rodriguez@upec.edu.ec>
2026-06-03 22:09:39 -0500

    clean: eliminar archivos obsoletos (documentacion TXT, scripts BAT redundantes, logs, previews)
    Eliminados 26 archivos:
    - 13 archivos de documentacion TXT (APP_ESCRITORIO_DESKTOP, COMIENZA_AQUI, ENTREGA_FINAL, ESTRUCTURA_VISUAL, INDICE, INSTRUCCIONES, QUICK_START, RESUMEN, TEST, YA_ESTA_LISTO, README, jar_contents, CHROMIS_ADMIN_STANDALONE)
    - 9 scripts BAT redundantes (run_chromispos, RUN, run_test, test_premium, jlink-pos, commit_push, INICIO_RAPIDO, EJECUTAR_AHORA, COMPILAR_Y_ABRIR)
    - chromis_admin_preview.html, debug.log, chromispos.log, logs/invoice/module.log
    
    Conservados los archivos esenciales:
    - ChromisPOS.exe.bat (launcher principal)
    - iniciar.bat, init_database.bat, schedule_backup.bat
    - build.bat, compile.bat, compile_premium.bat, compile_premium.ps1, compile_standalone.bat, build_clean.bat
    - ChromisEC_Installer.iss (instalador profesional)
    - README.md, VERSION.md (documentacion oficial)
    - chromisposconfig.properties (configuracion)

--- commit_push.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /D %~dp0
-git commit -m "feat: ChromisposEC v1.5.5 - Facturacion Electronica Ecuador SRI [Riccijandro] Motor XML Facturas y Notas de Credito, Algoritmo Clave Acceso SRI 49 digitos, Panel Gestion Facturas BD real, Anulacion legal via Notas de Credito, Configuracion SRI ambiente SMTP certificados, Script init BD Ecuador, Configuracion regional Ecuador. Autor: Riccijandro | richardrodriguez271@gmail.com | github.com/riccijandro | wa.me/593983185069"
-git push origin master
-echo.
-echo Commit y Push completados!
-pause
--- jlink-pos.bat
+++
@@ -1,6 +0,0 @@
-
-rem jdeps --class-path ./dist/lib/*  -recursive --multi-release 11 ./dist/lib/chromispos.jar > files.txt
-
-rmdir /s /q jre
-
-jlink --add-modules javafx.base,javafx.controls,javafx.swing,javafx.graphics,java.management,java.base,java.desktop,java.logging,java.xml,java.sql,java.rmi,java.naming,java.prefs --output jre --compress=2 --no-header-files --no-man-pages --strip-debug
